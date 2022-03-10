47cda056e9d2f1791878e34bc2b464af1a97db85
artakoroushnia <arta.kouroshnia@thredapps.com>
2022-03-10 03:02:08 -0500

    Merge Branches

--- ThredMarketplace/ThredMarketplace.sol
+++ ThredMarketplace/ThredMarketplace.sol
@@ -1,33 +1,31 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.7.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@thredapps/contracts/ERC721Merchant/ERC721Merchant.sol";
 
 pragma abicoder v2;
 
-contract ERC721FancyMerchant is
+contract ThredMerchant is
     ReentrancyGuard,
-    ERC721URIStorage,
     EIP712,
-    AccessControl,
-    Ownable
+    AccessControl
 {
     using Counters for Counters.Counter;
     using SafeMath for uint256;
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bool publicContract = false;
     Counters.Counter private _itemIds;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     struct MarketItem {
         uint256 itemId;
@@ -61,7 +59,7 @@ contract ERC721FancyMerchant is
     ) anonymous;
 
     event ListedNFT(
-        bool indexed listed,
+        address indexed contractId,
         uint256 indexed price,
         address indexed from,
         uint256 indexed tokenId
@@ -69,52 +67,76 @@ contract ERC721FancyMerchant is
 
     mapping(uint256 => MarketItem) private idToMarketItem;
 
-    function mintAndTransferCustom(NFTVoucher calldata voucher)
+    function mintAndTransferCustom(NFTVoucher calldata voucher, address contractAddress)
         public
         nonReentrant
         returns (bool)
     {
         require(voucher.minPrice > 0, "Price must be at least 1 wei");
-        address signer = redeem(msg.sender, voucher.minPrice, voucher);
-        return createMarketItem(payable(signer), payable(msg.sender), voucher);
+        address signer = _verify(voucher);
+        require(
+                hasRole(MINTER_ROLE, signer),
+                "Signature invalid or unauthorized"
+        );
+
+        IERC721Merchant nft = IERC721Merchant(contractAddress);
+
+        nft.mint(msg.sender, voucher.tokenId, voucher.uri);
+        
+        return createMarketItem(payable(signer), payable(msg.sender), contractAddress, voucher);
     }
 
-    function createSaleCustom(MarketItem memory item) public nonReentrant returns (bool) {
+    function createSale(MarketItem memory item) public nonReentrant returns (bool) {
         require(item.price > 0, "Price must be at least 1 wei");
         require(
             item.price == idToMarketItem[item.itemId].price,
             "Price must match"
         );
+        require(
+            item.tokenId == idToMarketItem[item.itemId].tokenId,
+            "Token must match"
+        );
         MarketItem storage currentItem = idToMarketItem[item.itemId];
         require(currentItem.forSale == true, "Not for sale");
         address walletAddress = getThredAddress();
-        address seller = ownerOf(currentItem.tokenId);
-        address payable owner = currentItem.owner;
-        address tokenContract = currentItem.tokenContract;
+        address seller = currentItem.seller;
         uint256 price = currentItem.price;
         uint256 royalty = currentItem.royalty;
         uint256 fee = calculateFee(price, 3);
         uint256 royalties = calculateFee(price, royalty);
         uint256 calculated = calculateFee(price, 97 - royalty);
-        IERC20 paymentToken = IERC20(tokenContract);
-        require(
-            paymentToken.allowance(msg.sender, address(this)) >= price,
-            "Insufficient Allowance"
-        );
-        require(
-            paymentToken.transferFrom(msg.sender, seller, calculated),
-            "transfer Failed"
-        );
-        require(
-            paymentToken.transferFrom(msg.sender, owner, royalties),
-            "transfer Failed"
-        );
-        require(
-            paymentToken.transferFrom(msg.sender, walletAddress, fee),
-            "transfer Failed"
-        );
-        emit Sale(price, seller, msg.sender, currentItem.tokenId);
-        _transfer(seller, msg.sender, currentItem.tokenId);
+        address owner = currentItem.owner;
+
+        IERC721 nft = IERC721(currentItem.nftContract);
+
+        if (currentItem.isNative || currentItem.tokenContract == address(0)){
+            payable(seller).transfer(calculated);
+            payable(walletAddress).transfer(fee);
+        }
+        else{
+            IERC20 paymentToken = IERC20(currentItem.tokenContract);
+            if (
+                paymentToken.allowance(msg.sender, address(this)) >=
+                currentItem.price
+            ) {
+                require(
+                    paymentToken.transferFrom(msg.sender, seller, calculated),
+                    "transfer Failed"
+                );
+                require(
+                    paymentToken.transferFrom(msg.sender, walletAddress, fee),
+                    "transfer Failed"
+                );
+                require(
+                    paymentToken.transferFrom(msg.sender, owner, royalties),
+                    "transfer Failed"
+                );
+            } else {
+                revert("Insufficient Allowance");
+            }
+        }
+        // emit Sale(price, seller, msg.sender, currentItem.tokenId);
+        nft.transferFrom(seller, msg.sender, currentItem.tokenId);
         idToMarketItem[item.itemId].seller = payable(msg.sender);
         idToMarketItem[item.itemId].forSale = false;
         return true;
@@ -131,17 +153,11 @@ contract ERC721FancyMerchant is
     }
 
     constructor(
-        string memory tknName,
-        string memory tknSymbol,
         string memory domain,
         string memory version,
-        address minter
-    ) ERC721(tknName, tknSymbol) EIP712(domain, version) {
-        if (minter != address(0)) {
-            _setupRole(MINTER_ROLE, minter);
-        } else {
-            publicContract = true;
-        }
+        address minter 
+    ) EIP712(domain, version) {
+        _setupRole(MINTER_ROLE, minter);
     }
 
     function getThredAddress() internal pure returns (address) {
@@ -157,7 +173,8 @@ contract ERC721FancyMerchant is
             item.tokenId == idToMarketItem[item.itemId].tokenId,
             "tokenId must match"
         );
-        address seller = ownerOf(idToMarketItem[item.itemId].tokenId);
+        IERC721 nft = IERC721(idToMarketItem[item.itemId].nftContract);
+        address seller = nft.ownerOf(idToMarketItem[item.itemId].tokenId);
         require(
             msg.sender == seller,
             "sender not proper seller"
@@ -169,58 +186,11 @@ contract ERC721FancyMerchant is
         idToMarketItem[item.itemId].seller = payable(seller);
         
         if (forSale == true){
-          emit ListedNFT(true, price, msg.sender, item.tokenId);
+            emit ListedNFT(idToMarketItem[item.itemId].nftContract, price, msg.sender, item.tokenId);
         }
         return true;
     }
 
-    function redeem(
-        address redeemer,
-        uint256 price,
-        NFTVoucher calldata voucher
-    ) internal returns (address) {
-        require(voucher.minPrice > 0, "Price must be at least 1 wei");
-        require(price >= voucher.minPrice, "Insufficient funds to redeem");
-        address walletAddress = getThredAddress();
-        address signer = _verify(voucher);
-        if (!publicContract) {
-            require(
-                hasRole(MINTER_ROLE, signer),
-                "Signature invalid or unauthorized"
-            );
-        }
-        _mint(signer, voucher.tokenId);
-        _setTokenURI(voucher.tokenId, voucher.uri);
-        emit ListedNFT(true, price, signer, voucher.tokenId);
-        uint256 calculated = calculateFee(voucher.minPrice, 97);
-        uint256 fee = calculateFee(voucher.minPrice, 3);
-
-        if (voucher.isNative || voucher.token == address(0)) {
-            payable(signer).transfer(calculated);
-            payable(walletAddress).transfer(fee);
-        } else {
-            IERC20 paymentToken = IERC20(voucher.token);
-            if (
-                paymentToken.allowance(redeemer, address(this)) >=
-                voucher.minPrice
-            ) {
-                require(
-                    paymentToken.transferFrom(redeemer, signer, calculated),
-                    "transfer Failed"
-                );
-                require(
-                    paymentToken.transferFrom(redeemer, walletAddress, fee),
-                    "transfer Failed"
-                );
-            } else {
-                revert("Insufficient Allowance");
-            }
-        }
-
-        emit Sale(voucher.minPrice, signer, redeemer, voucher.tokenId);
-        _transfer(signer, redeemer, voucher.tokenId);
-        return signer;
-    }
 
     function _hash(NFTVoucher calldata voucher)
         internal
@@ -262,31 +232,20 @@ contract ERC721FancyMerchant is
         return ECDSA.recover(digest, voucher.signature);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        virtual
-        override(AccessControl, ERC721)
-        view
-        returns (bool)
-    {
-        return
-            ERC721.supportsInterface(interfaceId) ||
-            AccessControl.supportsInterface(interfaceId);
-    }
-
     /* Places an item for sale on the marketplace */
     function createMarketItem(
         address payable nftOwner,
         address payable nftSeller,
+        address nftContract,
         NFTVoucher calldata voucher
-    ) public returns (bool) {
+    ) internal returns (bool) {
         require(voucher.minPrice > 0, "Price must be at least 1 wei");
         // require(msg.value == listingPrice, "Price must be equal to listing price");
         _itemIds.increment();
         uint256 itemId = _itemIds.current();
         idToMarketItem[itemId] = MarketItem(
             itemId,
-            address(this),
+            nftContract,
             voucher.tokenId,
             nftSeller,
             nftOwner,
@@ -297,42 +256,46 @@ contract ERC721FancyMerchant is
             voucher.isNative,
             true
         );
+
+        emit ListedNFT(nftContract, voucher.minPrice, nftOwner, voucher.tokenId);
+
         return true;
     }
 
     function mintNFT(
-        NFTVoucher calldata voucher
+        NFTVoucher calldata voucher,
+        address nftContract
     ) public returns (bool) {
-        if (!publicContract) {
-            require(
-                hasRole(MINTER_ROLE, msg.sender),
-                "invalid or unauthorized"
-            );
-        }
+        require(
+            hasRole(MINTER_ROLE, msg.sender),
+            "invalid or unauthorized"
+        );
         require(voucher.minPrice > 0, "Price must be at least 1 wei");
-        _mint(msg.sender, voucher.tokenId);
-        _setTokenURI(voucher.tokenId, voucher.uri);
-        bool created = createMarketItem(payable(msg.sender), payable(msg.sender), voucher);
+
+        IERC721Merchant nft = IERC721Merchant(nftContract);
+
+        nft.mint(msg.sender, voucher.tokenId, voucher.uri);
+
+        bool created = createMarketItem(payable(msg.sender), payable(msg.sender), nftContract, voucher);
         if (created){
             uint256 itemId = _itemIds.current();
             idToMarketItem[itemId].forSale = true;
-            emit ListedNFT(true, voucher.minPrice, msg.sender, voucher.tokenId);
         }
-        return true;
+        return created;
     }
 
-    function fetchMarketItems() public view returns (MarketItem[] memory) {
+    function fetchMarketItems(address contractAddress) public view returns (MarketItem[] memory) {
         uint256 itemCount = _itemIds.current();
         uint256 currentIndex = 0;
 
         MarketItem[] memory items = new MarketItem[](itemCount);
         for (uint256 i = 0; i < itemCount; i++) {
-            // if (idToMarketItem[i + 1].owner == address(0)) {
+            if (idToMarketItem[i + 1].nftContract == contractAddress) {
             uint256 currentId = i + 1;
             MarketItem storage currentItem = idToMarketItem[currentId];
             items[currentIndex] = currentItem;
             currentIndex += 1;
-            // }
+            }
         }
         return items;
     }
